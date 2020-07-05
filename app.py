979766ff9e17425227da88bd4d0370592bb761ef
hamza windows <you@example.com>
2020-07-05 09:39:06 +0100

    add post and git option to /api/gen/uid

--- app.py
+++ app.py
@@ -4,16 +4,15 @@ from firebase import store_account
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/',methods=['GET','POST'])
 def hello_world():
     return generate_account_info()
 
 
-@app.route('/api/gen/<uid>')
+@app.route('/api/gen/<uid>',methods=['GET','POST'] )
 def store_fire_base(uid):
     store_account(uid,generate_account_info())
     return "succeed"
-
 
 if __name__ == '__main__':
     app.run(debug=True)
